c448c01e40b8a7b60371c403a9e9c9056e50dec1
Your Name <you@example.com>
2018-08-22 21:39:39 +0100

    payment + calendar

--- app/code/local/Silk/Retailer/Model/Method/Free.php
+++ app/code/local/Silk/Retailer/Model/Method/Free.php
@@ -3,7 +3,16 @@ class Silk_Retailer_Model_Method_Free extends Mage_Payment_Model_Method_Free{
 	public function isAvailable($quote = null)
 	{
 		if(Mage::getSingleton('customer/session')->isLoggedIn() && Mage::helper('epicor_comm')->getErpAccountInfo()->getAccountType() == 'B2B'){
-			return false;
+			//return true;
+			$quote = Mage::getModel('checkout/session')->getQuote();
+			$quoteData= $quote->getData();
+			$grandTotal=$quoteData['grand_total'];
+			if($grandTotal == 0){
+				return true;
+			}
+			else{
+				return false;
+			} 
 		}
 		return true;
 	}
